62c73d9452729313eb57c75914601ed4b3b31eab
phoenix <vanguelisk@gmail.com>
2019-01-08 08:19:22 +0200

    native query

--- src/main/java/hello/PerformancesController.java
+++ src/main/java/hello/PerformancesController.java
@@ -18,7 +18,41 @@ public class PerformancesController {
 
 
     @RequestMapping("/api/performances")
-    public String getPerformances()
+    public String getPerformancesList()
+    {
+        Session session = null;
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+
+            String q =  " SELECT p.per_duration, pl.ppl_fullname, t.tht_name, " +
+            "  ply.ply_title, a.aut_name " +
+            " FROM  performance p " +
+            " JOIN people pl   ON  pl.ppl_id = per_director_id " +
+            " JOIN theaters t ON T.tht_id = p.per_theater_id " + 
+            " JOIN plays ply ON ply.ply_id =  per_play_id " + 
+            " JOIN authors a ON a.aut_id = ply.ply_author_id " +
+            " WHERE  CURRENT_DATE >= p.per_from_date  " +
+            " AND  p.per_to_date >= CURRENT_DATE ";
+            List<Object[]> persons = session.createNativeQuery(q)
+            .getResultList();
+
+           return  ow.writeValueAsString(persons);
+
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+
+            return ex.getMessage();
+        }
+    }
+
+
+
+    @RequestMapping("/api/performance")
+    public String getPerformance()
     {
         Session session = null;
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();

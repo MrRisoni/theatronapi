33fc63821902c0d0dc50c241a014c4995841a8b3
phoenix <vanguelisk@gmail.com>
2019-01-18 09:41:30 +0200

    post example

--- fakeNodejsBankAPI/postData.js
+++ fakeNodejsBankAPI/postData.js
@@ -1,31 +1,50 @@
-const axios  = require('axios');
-
-const ppl = [
-{
+const axios = require('axios');
 
 
-          type: 'ADT',
-          seat: 'AVC',
-        },
-          {
+const postObj = {
+    people: [{
 
-                  type: 'ADT',
-                  seat: 'ABD',
+                    type: 'ADT',
+                    seat: 'AVC',
                 },
-]
+                {
 
- axios.post('http://localhost:8080/api/book',  {
-          people:ppl})
-          .then((responseObj) => {
-              const responseData = responseObj.data;
+                    type: 'ADT',
+                    seat: 'ABD',
+                },
+            ],
+    credit: {
+        number: '411111111111111',
+        cvv: '123',
+        type: 'VIDE',
+        cardHolder: 'ARTHUR MILLER',
+        expMonth: '12',
+        expYear: '22',
+    },
+    performance: {
+        id: 1,
+        date: '2019-01-25'
+    },
+    contact: {
+        mail: 'test@test.gr',
+        surname: 'CHEKOV',
+        name: 'ANTON',
+        gender: 'M',
+        prefix: '30',
+        mobile: '1234567890'
+    }
+}
+
+axios.post('http://localhost:8080/api/book', postObj)
+    .then((responseObj) => {
+        const responseData = responseObj.data;
 
 
 
 
-          }).catch((err) => {
+    }).catch((err) => {
 
 
 
 
-      });
-
+    });

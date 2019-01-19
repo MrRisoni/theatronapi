1e4979e86f66cbd17cecb736f256ae2174e4f0e7
phoenix <vanguelisk@gmail.com>
2019-01-19 15:18:45 +0200

    theater seats total seats

--- queries.sql
+++ queries.sql
@@ -1,11 +1,15 @@
 SELECT t.tht_id AS theaterId, p.per_id AS performanceId, p.per_duration AS duration, t.tht_name AS theaterName,  p.per_from_date AS fromDate, p.per_to_date AS toDate,
-                      ply.ply_title AS playName , G.gen_title
+                      ply.ply_title AS playName , G.gen_title, theaterSeats.*
                      FROM  performance p
                      JOIN people pl   ON  pl.ppl_id = per_director_id
                      JOIN theaters t ON T.tht_id = p.per_theater_id
                      JOIN plays ply ON ply.ply_id =  per_play_id
                      JOIN authors a ON a.aut_id = ply.ply_author_id
                      JOIN genres G ON G.gen_id = ply.ply_genre_id
+
+          JOIN ( SELECT smp_theater_id, COUNT(smp_id) AS totalSeats
+                 FROM  seatmap
+                 GROUP BY smp_theater_id ) AS theaterSeats ON  theaterSeats.smp_theater_id = p.per_theater_id
 
 
 
@@ -17,20 +21,18 @@ GROUP BY p.per_id
 
 
 
-          JOIN ( SELECT smp_theater_id, COUNT(smp_id) AS totalSeats
-                 FROM  seatmap
-                 GROUP BY smp_theater_id ) AS theaterSeats ON  theaterSeats.smp_theater_id = p.per_theater_id
-
 
 
 // average tickets per day
- SELECT 	p.per_id ,
+
+
+ SELECT 	p.per_id,theaterSeats.totalSeats,
  AVG(IF ( orderItems.tickets IS NULL,0,orderItems.tickets)) AS totalTickets
           FROM  performance_dates
           JOIN performance p ON (p.per_id = prd_performance_id AND p.per_season_id = prd_season_id)
-
-
-
+JOIN ( SELECT smp_theater_id, COUNT(smp_id) AS totalSeats
+                 FROM  seatmap
+                 GROUP BY smp_theater_id ) AS theaterSeats ON  theaterSeats.smp_theater_id = p.per_theater_id
           LEFT JOIN (
                   SELECT  ord_perf_date_id, COUNT(itm_id) AS tickets  FROM order_item
                   JOIN orders ON itm_order_id = ord_id

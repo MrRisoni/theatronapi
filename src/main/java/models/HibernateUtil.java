95a423f9be4abb53ecf65d053456df0c27044e50
MrRisoni <vanguelisk@gmail.com>
2020-07-17 18:57:29 +0300

    entity manager config

--- src/main/java/models/HibernateUtil.java
+++ src/main/java/models/HibernateUtil.java
@@ -1,22 +1,45 @@
 package models;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HibernateUtil {
 
     private static SessionFactory sessionFactory;
 
+    private static EntityManager em = null;
+
+    public static  EntityManager getEM()
+    {
+        if (em == null) {
+            Map<String, Object> configOverrides = new HashMap<String, Object>();
+            System.out.println("SYSTEM ENV");
+            configOverrides.put("javax.persistence.jdbc.password", System.getenv("SPRING_APP_DB_PASSWD"));
+
+            EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("theatrondb",configOverrides);
+            em = entityManagerFactory.createEntityManager();
+        }
+        return  em;
+    }
 
     public static SessionFactory buildSessionFactory() {
         // A SessionFactory is set up once for an application!
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
-                .build();
+
+        Map<String,String> HerokuSettings = new HashMap<>();
+        HerokuSettings.put("hibernate.connection.password",System.getenv("SPRING_APP_DB_PASSWD"));
+
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().
+                configure("hibernate.cfg.xml").
+                applySettings(HerokuSettings).
+                build();
         try {
             sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
             return sessionFactory;
